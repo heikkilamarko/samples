bdc67d3ff9a5bc48421046113e4f97b8dd1f8070
Marko Heikkilä <heikkila_marko@hotmail.com>
2021-09-02 10:52:44 +0300

    Add SPA routing sample.

---
+++ dotnet/spa-routing/README.md
@@ -0,0 +1,3 @@
+# SPA Routing
+
+This is needed when both the UI and API are served from the same .NET project.
---
+++ dotnet/spa-routing/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSpaStaticFiles(config =>
+            {
+                config.RootPath = "TODO: set root path here";
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSpaStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    if (ctx.Context.Request.Path.StartsWithSegments("/assets"))
+                    {
+                        ctx.Context.Response.Headers.Add("Cache-Control", "max-age=31536000");
+                    }
+                }
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions
+                {
+                    OnPrepareResponse = ctx =>
+                    {
+                        if (ctx.Context.Request.Path == "/index.html")
+                        {
+                            ctx.Context.Response.Headers.Add("Cache-Control", "no-store, max-age=0");
+                        }
+                    }
+                };
+            });
+        }
+    }
+}
